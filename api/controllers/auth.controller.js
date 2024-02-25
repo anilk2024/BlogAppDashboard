1660020f792195a062fb3a4c0bb9d3edc376a5b4
anilk2022 <pfms.anilkumar@gmail.com>
2024-02-25 12:45:16 +0530

    created middileware to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,23 +1,33 @@
 import User from "../models/user.model.js";
+import { errorHandler } from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: 'All fields are required' })
-
+  
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === '' ||
+      email === '' ||
+      password === ''
+    ) {
+      next(errorHandler(400, 'All fields are required'));
     }
-    const hashedPassword=bcryptjs.hashSync(password,10);
+  
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+  
     const newUser = new User({
-        username,
-        email,
-        password:hashedPassword
-    })
+      username,
+      email,
+      password: hashedPassword,
+    });
+  
     try {
-        await newUser.save();
-        res.status(201).json({ message: 'SignUp successful' })
+      await newUser.save();
+      res.status(201).json('Signup successful');
     } catch (error) {
-        res.status(500).json({ message: error.message })
+      next(error);
     }
-    res.status(201).json({ message: 'register successfull' })
-}
+  };
